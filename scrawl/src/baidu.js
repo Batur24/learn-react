55f4c8b81b8e06c2353eda517aa35b07bae96296
Batur <heshupai@gmail.com>
2016-08-06 02:22:35 +0800

    优化百度页面

--- scrawl/src/baidu.js
+++ scrawl/src/baidu.js
@@ -2,6 +2,12 @@ import React from 'react'
 
 var Baidu = React.createClass({
     render(){
+		var baiduNavStyle = {
+								position: "absolute",
+								top: 190,
+								right: 20,
+								display: "block",
+							};
         var baiduStyle = {
                             textAlign: "center",
                             marginTop: 200
@@ -10,11 +16,39 @@ var Baidu = React.createClass({
                             width: 300,
                             height: 20
                         };
+        var btnStyle = {
+							backgroundColor: "#38f",
+							color: "white",
+                        };
+		var baiduLink = {
+							paddingLeft: 10,
+							color: "black"
+						};
+		var moreProduct = {
+							marginLeft: 10,
+							width: 80,	
+							height: 30,	
+							backgroundColor: "#38f",
+							color: "white"
+						  };
         return (
-            <div style={baiduStyle}>
-                <input type="text" style={textStyle}/>
-                <input type="button" value="百度一下" />
-            </div>
+			<div>
+				<div id="baidu-nav" style={baiduNavStyle}>
+					<a href="#"  style={baiduLink} >糯米</a>
+					<a href="#"  style={baiduLink} >新闻</a>
+					<a href="#"  style={baiduLink} >hao123</a>
+					<a href="#"  style={baiduLink} >地图</a>
+					<a href="#"  style={baiduLink} >视频</a>
+					<a href="#"  style={baiduLink} >贴吧</a>
+					<a href="#"  style={baiduLink} >登录</a>
+					<a href="#"  style={baiduLink} >设置</a>
+					<button style={moreProduct} >更多产品</button>
+				</div>
+				<div style={baiduStyle}>
+					<input type="text" style={textStyle}/>
+					<input type="button" value="百度一下" style={btnStyle}/>
+				</div>
+			</div>
             )
     }
 })
